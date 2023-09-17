02c55146995945efbddc44a22c0577203740f96d
Njabulo Mdluli <82125948+Njabulorivaldo@users.noreply.github.com>
2023-09-17 10:05:20 +0200

    Update models.py

--- website/models.py
+++ website/models.py
@@ -1,14 +1,30 @@
 from . import db 
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 class Content(db.Model):
-    id = db.Column(db.Integer)
-    type = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    file_type = db.Column(db.String(150))
+    date_added = db.Column(db.DateTime(timezone=True), default=func.now())
+    file_path = db.Column(db.String(10000))
 
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content_id = db.Column(db.Integer, db.ForeignKey("content.id"))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+
+class Screens(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content_id = db.Column(db.Integer, db.ForeignKey("content.id"))
+    location = db.Column(db.String(150))
+    status = db.Column(db.String(150)) # Offline/ Online/ Maintenance
+    contents = db.relationship("Content")
 
 class User(db.Model, UserMixin):
-
-    user_id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(150), unique = True)
     password = db.Column(db.String(150))
     department = db.Column(db.String(150))
+    contents = db.relationship("Content")
