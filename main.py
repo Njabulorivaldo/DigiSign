66e0181b3f069d87945de57e4730f08ca78c220e
Njabulo Mdluli <82125948+Njabulorivaldo@users.noreply.github.com>
2023-09-24 19:01:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,8 @@ from flask import Flask, render_template
 
 def run_app():
     app = create_app()
-    app.run(debug=True)
-
-
-#@app.route('/')
-#def home():
-#    return render_template('index.html')
+    #app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug="True")
 
 
 if __name__ == "__main__":
