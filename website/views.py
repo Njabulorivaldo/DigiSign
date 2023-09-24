0d8ec132739dc0292e5bd7e326f370a9f0e63b04
Njabulo Mdluli <82125948+Njabulorivaldo@users.noreply.github.com>
2023-09-24 19:03:10 +0200

    Update views.py

--- website/views.py
+++ website/views.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from .models import Content
-
+from . import db
 from website import create_app
+import json
 
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
@@ -13,6 +14,8 @@ from wtforms.validators import InputRequired
 views = Blueprint("views", __name__)
 #Blueprint means it contains a bunch of routes inside it
 
+
+
 class UploadFileForm(FlaskForm):
     file = FileField("File", validators=[InputRequired()])
     submit = SubmitField("Upload File")
@@ -20,61 +23,93 @@ class UploadFileForm(FlaskForm):
 
 def get_type(path):
     
-    type = path[-4::] 
-    print(type)
-    audio_types = [".mp3",".wav",".flac",".ogg",".aac",".m4a",".wma",".aiff",".au",".ra",".mid",".midi",".amr",".ac3",".opus",".mka",".webm",".caf",".pcm",]
-    images_types = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp']
-    video_types = ['.mp4', '.mkv', '.avi', '.mov', '.wmv', '.flv', '.webm']
+    type = path[-4::].lower()
+    images_types = ['.jpg', 'jpeg', '.png', '.gif', '.bmp', 'tiff', 'webp']
+    video_types = ['.mp4', '.avi', '.mkv', '.mov', '.wmv', '.flv', '.m4v', '.ogg', '.ogv', '.mpg']
 
-    if type in audio_types:
+    
+    if type == ".mp3":
         return "Audio"
+    
     elif type in video_types:
         return "Video"
+    
     elif type in images_types:
         return "Image"
+    
     else:
         return None
+    
+def makeUserPath(dir):
+    try: 
+        os.mkdir(dir) 
+    except OSError as error: 
+        #User path already exists
+        pass 
+
+
+@views.route('/profile', methods=["GET", "POST"])
+@login_required
+def profile():
+    return render_template('profile.html', user = current_user)
 
 @views.route('/', methods=["GET", "POST"])
 @login_required
-def home():
+def dashboard():
+    return render_template('dashboard.html', user = current_user)
+
+
+@views.route('/screens', methods=["GET", "POST"])
+@login_required
+def screens():
+    return render_template('screens.html', user = current_user)
+
+
+@views.route("/media", methods=["GET", "POST"])
+@login_required   
+def media():
     app = create_app()
     form = UploadFileForm()
-    
+    message=""
     if form.validate_on_submit():
         file = form.file.data # First grab the file
-        file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))
 
+        #Get the necessary paths for user and file
+        file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'] +"/"+ str(current_user.id),secure_filename(file.filename))
+        user_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'], str(current_user.id))
+        dir_name = os.path.join(app.config['UPLOAD_FOLDER'], str(current_user.id), secure_filename(file.filename))
+        
+        #Checks if the user has a folder alreay existing, if not make a dir to store their files
+        makeUserPath(user_dir)
+        
         #Check if file is one of the accepted types/extensitons
-        type = get_type(file_path)
-        print(type)
+        type = get_type(dir_name)
+
 
         if type == None:
-            flash("You Can Only Upload Audio/Video/Image", category="error")
-              
+            flash("You Can Only Upload Audio.mp3/Video.mp4/Image", category="error")
+            message= "Unsupported Format"
+        
         else:
             file.save(file_path) # Then save the file
-            flash("Successfully Uploaded.", category="success")
+            new_content = Content(user_id=current_user.id, file_type=type, file_path=dir_name)
+            db.session.add(new_content)
+            db.session.commit()
+            #flash("Successfully Uploaded.", category="success")
+            message='Successfully Uploaded.'
+    return render_template("media.html", user=current_user, form=form, msg=message)
 
-
-        #Getting the file type
-
-
-        #new_content = Content(user_id=current_user.id, 
-
-    return render_template('home.html', user = current_user, form=form)
-
-
-'''def home():
-    if request.method == "POST":
-        content = request.form.get("content")
-
-        if content:
-            new_content = Content()
-            flash("Content uploaded", category="success")
-            pass
-
-        else:
-            flash("Upload valid content", category="error")
-
-    return render_template("home.html", user=current_user)'''
+@views.route("/delete-content", methods=["POST"])
+def deleteContent():
+    content = json.loads(request.data)
+    contentID = content["contentID"]
+    content = Content.query.get(contentID)
+    print(content.file_path)
+    
+    if content:
+        if content.user_id == current_user.id:
+            db.session.delete(content)
+            db.session.commit()
+            os.remove("../DigitalSign/website/"+content.file_path)
+            
+    return render_template("media.html", user=current_user)
